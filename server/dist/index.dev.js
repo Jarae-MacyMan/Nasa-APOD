0c5306d4698b672c574c055dcd16a47056f9f46d
Jarae-MacyMan <sjaraebr@gmail.com>
2023-04-07 15:10:01 -0400

    server update

---
+++ server/dist/index.dev.js
@@ -0,0 +1,52 @@
+"use strict";
+
+require("dotenv").config();
+
+var express = require("express");
+
+var cors = require("cors");
+
+var passport = require("passport");
+
+var authRoute = require("./routes/auth");
+
+var userRoutes = require("./routes/userRoutes");
+
+var cookieSession = require("cookie-session");
+
+var passportStrategy = require("./passport");
+
+var mongoose = require('mongoose');
+
+require("dotenv").config();
+
+var bodyParser = require('body-parser');
+
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
+var app = express();
+app.use(cookieSession({
+  name: "session",
+  keys: ["sjar"],
+  maxAge: 24 * 60 * 60 * 100
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
+app.use("/auth", authRoute);
+app.use("/users", userRoutes);
+var MONGOOSE_URL = process.env.MONGOOSE_URL;
+var port = process.env.PORT || 8080;
+mongoose.connect(MONGOOSE_URL, {
+  useNewUrlParser: true
+}).then(function () {
+  return app.listen(port, function () {
+    console.log("Server is running at port ".concat(port));
+  });
+});
