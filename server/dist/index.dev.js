6ef635d2f7db91b5f8ca253504f8caf9a210b14d
Jarae-MacyMan <sjaraebr@gmail.com>
2023-04-07 17:07:19 -0400

    server vercel cors update

--- server/dist/index.dev.js
+++ server/dist/index.dev.js
@@ -35,7 +35,7 @@ app.use(cookieSession({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://nasa-apod-apis.vercel.app",
   methods: "GET,POST,PUT,DELETE",
   credentials: true
 }));
