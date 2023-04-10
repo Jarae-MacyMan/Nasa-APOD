ebee5976dcee997359a5770f011b6f7f53deb8ea
Jarae-MacyMan <sjaraebr@gmail.com>
2023-04-09 23:43:40 -0400

    vercel req updates 28

--- client/src/api/dist/index.dev.js
+++ client/src/api/dist/index.dev.js
@@ -1,18 +1,11 @@
-"use strict";
-
-var _axios = _interopRequireDefault(require("axios"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var API = _axios["default"].create({
-  baseURL: "https://nasa-apod-apis.vercel.app"
-});
-
-API.interceptors.request.use(function (req) {
-  if (localStorage.getItem("user_info")) {
-    req.headers.Authorization = "Bearer ".concat(JSON.parse(localStorage.getItem("user_info").token));
-  }
-
-  return req;
-}); // export const signIn = (data) => API.post("/users/signin", data)
+// import axios from "axios"
+// const API = axios.create({baseURL:"https://nasa-apod-apis.vercel.app"})
+// API.interceptors.request.use((req)=>{
+//     if(localStorage.getItem("user_info")){
+//         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("user_info").token)}`
+//     }
+//     return req;
+// })
+// export const signIn = (data) => API.post("/users/signin", data)
 // export const signUp = (data) => API.post("/users/signup", data)
+"use strict";
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,14 +1,14 @@
-import axios from "axios"
+// import axios from "axios"
 
-const API = axios.create({baseURL:"https://nasa-apod-apis.vercel.app"})
+// const API = axios.create({baseURL:"https://nasa-apod-apis.vercel.app"})
 
-API.interceptors.request.use((req)=>{
-    if(localStorage.getItem("user_info")){
-        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("user_info").token)}`
-    }
+// API.interceptors.request.use((req)=>{
+//     if(localStorage.getItem("user_info")){
+//         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("user_info").token)}`
+//     }
 
-    return req;
-})
+//     return req;
+// })
 
 // export const signIn = (data) => API.post("/users/signin", data)
 
