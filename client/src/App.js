a80fa60228ec169e669d8c853154b225835c3d88
Jarae-MacyMan <sjaraebr@gmail.com>
2023-04-10 02:07:15 -0400

    vercel req updates 39

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
 		try {
 			const url = `https://nasa-apod-apis.vercel.app/auth/login/success`;
 			const { data } = await axios.get(url, { withCredentials: true });
-				context.setUser(data.user);
+				context.setUser(data.user._json);
 		} catch (err) {
 			console.log(err);
 		}
