f82799dd55a4a5834fc53d0d4f811b8023d17147
Jarae-MacyMan <sjaraebr@gmail.com>
2023-04-09 20:21:24 -0400

    vercel req updates 9

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(
 
 app.use(
 	cors({
-		origin: ["https://nasa-apod-rho.vercel.app", "http://localhost:3000", "http://127.0.0.1:3000"],
+		origin: "*",
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
